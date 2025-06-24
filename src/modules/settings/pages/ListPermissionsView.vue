7cd5b6419e1157bd1e9cca8f34187e59e36781c3
Ademir Rivera <uarivera@innovacion.gob.sv>
2025-06-24 14:29:51 -0600

    feat: maquetando la vista de permisos

---
+++ src/modules/settings/pages/ListPermissionsView.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container fluid>
+    <!-- title -->
+    <div class="d-flex justify-center justify-md-start">
+      <span class="text-h4 text-sm-h3 text-primary"> Permisos </span>
+    </div>
+
+    <!-- table -->
+    <v-data-table-server
+      :headers="headers"
+      :items="permissionsQuery.data.value?.data || []"
+      :items-per-page="paramsPermissions.perPage"
+      :page="paramsPermissions.page"
+      :items-length="totalItems"
+      :loading="permissionsQuery.isLoading.value"
+      @update:options="onPaginate"
+    />
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import type { DataTableServerOptions } from '@/interfaces/vuetify.interfaces'
+import settingsServices from '@/modules/settings/settings.services'
+
+const headers = [
+  { title: 'Nombre', key: 'name', sortable: false },
+  { title: 'Descripción', key: 'description', sortable: false }
+]
+
+const paramsPermissions = reactive({
+  page: 1,
+  perPage: 10
+})
+
+const totalItems = computed(
+  () => permissionsQuery.data.value?.pagination.total || 0
+)
+
+const permissionsQuery = useQuery({
+  queryKey: ['list_permissions', paramsPermissions],
+  queryFn: () =>
+    settingsServices.getListPermissions({
+      page: paramsPermissions.page,
+      per_page: paramsPermissions.perPage
+    })
+})
+
+const onPaginate = (params: DataTableServerOptions) => {
+  paramsPermissions.page = params.page
+  paramsPermissions.perPage = params.itemsPerPage
+}
+</script>
